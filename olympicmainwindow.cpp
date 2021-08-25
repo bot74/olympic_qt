738a8a536ab0aeaf3d423ae79f587a9ec7a732fb
bot74 <41332266+bot74@users.noreply.github.com>
2021-08-26 03:15:14 +0800

    重做文件输入输出逻辑，定义输入输出数据格式
    修改了文件输入输出的格式，重做相关函数
    文件格式定义如下：
    第 1 行 3个整数国家编号 n,男子项目总数 m,女子项目总数 w，用/t分隔
    第 2 行到第 m + 1 行，每行开头一个对应的项目名称，之后是 5 个整数 Cij，表示对应的项目 mi 取前五的国家编号 Cij，用/t分隔
    第 m + 2 行到第 m + w + 1行，每行开头一个对应的项目名称，之后是 3 个整数 Cij, 表示对应的项目 wi 取前三的国家编号 Cij，用/t分隔
    规定 n >= 5 && n <= 300, m >= 0, w >= 0

--- olympicmainwindow.cpp
+++ olympicmainwindow.cpp
@@ -14,73 +14,40 @@ OlympicMainWindow::OlympicMainWindow(QWidget *parent)
     , ui(new Ui::OlympicMainWindow)
 {
     ui->setupUi(this);
-    //int index = 1;
+
+    //ui->tableWidgetScoreBoard->setColumnCount(4);动态调整列数的方法是setColumnCount(int)
 
     //建立初始表头
     QStringList header;
     header<<tr("CountryNum")<<tr("CountryScore")<<tr("MaleScore")<<tr("FemaleScore");
     ui->tableWidgetScoreBoard->setHorizontalHeaderLabels(header);
 
+    /*
+    header<<tr("我是动态第五列");
+    ui->tableWidgetScoreBoard->setColumnCount(5);
+    ui->tableWidgetScoreBoard->setHorizontalHeaderLabels(header);//证明了可以在设置完label以后再次重新修改
+    */
+
     qDebug() << "表头设置完毕";
     qDebug() << "表头为：" << header;
 
     //设置表格格式
     ui->tableWidgetScoreBoard->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
-    ui->tableWidgetScoreBoard->verticalHeader()->hide();
-    ui->tableWidgetScoreBoard->horizontalHeader()->setSortIndicatorShown(true);//This property holds whether the sort indicator is shown. By default, this property is false.
+    //ui->tableWidgetScoreBoard->verticalHeader()->hide();
+    //ui->tableWidgetScoreBoard->horizontalHeader()->setSortIndicatorShown(true);//This property holds whether the sort indicator is shown. By default, this property is false.
     ui->tableWidgetScoreBoard->horizontalHeader()->setSectionsClickable(true);//If clickable is true, the header will respond to single clicks.
     ui->tableWidgetScoreBoard->setEditTriggers(QAbstractItemView::NoEditTriggers);//初始主页表格禁用编辑
     //ui->tableWidgetScoreBoard.cell
+    for (int i = 4; i < 401; i++){//隐藏多余列
+        ui->tableWidgetScoreBoard->setColumnHidden(i, true);
+    }
 
 
     qDebug() << "表格样式设置完毕";
 
-    //从文件中读取数据，第一步，初始化
-    //QFile f("DefaultInput.txt");
-    curFile.setFileName("DefaultInput.txt");
-    if (! curFile.open(QIODevice::ReadOnly | QIODevice::Text)){
-        qDebug() << "读取文件失败";
-        qDebug()<<"current applicationDirPath: "<<QCoreApplication::applicationDirPath();
-        qDebug()<<"current currentPath: "<<QDir::currentPath();
-    }
-    QTextStream output(&curFile);
-    QHash<QString, QString> varHash;
-    QString lineStr;    //文件的每一行的字符串
-    QStringList lineList;   //整行字符串分割处理为单个字符串存入到表格中
-    varHash.clear();
-    lineList.clear();
-
-    //从文件中读取数据，第二步，遍历文件
-    //定义文件格式如下：
-    //第 1 行 3个整数国家编号 n,男子项目总数 m,女子项目总数 w，用/t分隔
-    //第 2 行到第 m + 1 行，每行 5 个整数 Cij，表示对应的项目 mi 取前五的国家编号 Cij，用/t分隔
-    //第 m + 2 行到第 m + w + 1行， 每行 3 个整数 Cij, 表示对应的项目 wi 取前三的国家编号 Cij，用/t分隔
-
-    //读取n,m,w并存储到对应变量中
-    //lineStr = output.readLine();
-    //lineList = lineStr.split('\t');
-    //int CountrySum = lineList[0].toInt();
-    //int MaleSum = lineList[1].toInt();
-    //int FemaleSum = lineList[2].toInt();
-
-    //读取各个小项目的具体奖牌分配情况
-    while(!output.atEnd()){
-        qDebug() << "循环中";
-        lineStr = output.readLine();  //读取文件的一行
-        qDebug() << lineStr;
-        lineList = lineStr.split('\t');   //去除lineStr中的换行符作为表格数据
-        //向表里面动态添加参数
-        int rowCount = ui->tableWidgetScoreBoard->rowCount();
-        ui->tableWidgetScoreBoard->insertRow(rowCount);
-        for (int i = 0; i < 4; i++){
-            //ui->tableWidgetScoreBoard->setItem(rowCount,i,new QTableWidgetItem(lineList[i]));   //使用QTable可以zheng'chang正常输出，使用我自己的则不行
-            ui->tableWidgetScoreBoard->setItem(rowCount,i,new TableWidgetItem(lineList[i]));//自己的继承重载版本
-            qDebug() << lineList[i];
-        }
-    }
-    curFile.close();//关闭文件
+    openDefault();
     connect(ui->tableWidgetScoreBoard->horizontalHeader(),SIGNAL(sectionClicked(int)),
-            this, SLOT(sortTableByCol(int)));
+            this, SLOT(sortTableByCol(int)));//用于排序的信号连接
 
 }
 
@@ -100,6 +67,7 @@ void OlympicMainWindow::sortTableByCol(int col)
     return;
 }
 
+
 OlympicMainWindow::~OlympicMainWindow()
 {
     delete ui;
@@ -110,7 +78,7 @@ void OlympicMainWindow::on_actionOpen_triggered()
 {
     //QString file_open;
     curFile.setFileName(QFileDialog::getOpenFileName(this,
-                            tr("Open Input Text"), "/", tr("Text files (*.txt)")));
+                                                     tr("Open Input Text"), "/", tr("Text files (*.txt)")));
     if (! curFile.open(QIODevice::ReadOnly | QIODevice::Text)){
         qDebug() << "读取文件失败";
         qDebug()<<"current applicationDirPath: "<<QCoreApplication::applicationDirPath();
@@ -127,19 +95,68 @@ void OlympicMainWindow::on_actionOpen_triggered()
     ui->tableWidgetScoreBoard->clearContents();
     ui->tableWidgetScoreBoard->setRowCount(0);
 
+    //从文件中读取数据，第一步，初始化
+    //QFile f("DefaultInput.txt");
+    curFile.setFileName("DefaultInput.txt");
+    if (! curFile.open(QIODevice::ReadOnly | QIODevice::Text)){
+        qDebug() << "读取文件失败";
+        qDebug()<<"current applicationDirPath: "<<QCoreApplication::applicationDirPath();
+        qDebug()<<"current currentPath: "<<QDir::currentPath();
+    }
+
     //从文件中读取数据，第二步，遍历文件
-    while(!output.atEnd()){
-        qDebug() << "循环中";
-        lineStr=output.readLine();  //读取文件的一行
-        qDebug() << lineStr;
-        lineList=lineStr.split('\t');   //去除lineStr中的换行符作为表格数据
-        //向表里面动态添加参数
+    //定义文件格式如下：
+    //第 1 行 3个整数国家编号 n,男子项目总数 m,女子项目总数 w，用/t分隔
+    //第 2 行到第 m + 1 行，每行开头一个对应的项目名称，之后是 5 个整数 Cij，表示对应的项目 mi 取前五的国家编号 Cij，用/t分隔
+    //第 m + 2 行到第 m + w + 1行，每行开头一个对应的项目名称，之后是 3 个整数 Cij, 表示对应的项目 wi 取前三的国家编号 Cij，用/t分隔
+    //规定 n >= 5 && n <= 300, m >= 0, w >= 0
+
+    //读取n,m,w并存储到对应变量中
+    lineStr = output.readLine();
+    lineList = lineStr.split('\t');
+
+    CountrySum = lineList[0].toInt();
+    MaleSum = lineList[1].toInt();
+    FemaleSum = lineList[2].toInt();
+    EventSum = MaleSum + FemaleSum;
+    int i = 0;
+    //QStringList EventList[400];//最多读取400行，放弃tempLine[0]不使用，也就是最多存储399个子项目数据
+    //int CountryScore[300];
+
+    //初始化国家分数
+    for (i=0;i<300;i++){
+        CountryScore[i] = 0;
+        CountryScoreFemale[i] = 0;
+        CountryScoreMale[i] = 0;
+    }
+
+    //根据国家总数添加对应行
+    for (i = 1; i <= CountrySum; i++){
         int rowCount = ui->tableWidgetScoreBoard->rowCount();
         ui->tableWidgetScoreBoard->insertRow(rowCount);
-        for (int i = 0; i < 4; i++){
-            //ui->tableWidgetScoreBoard->setItem(rowCount,i,new QTableWidgetItem(lineList[i]));   //使用QTable可以zheng'chang正常输出，使用我自己的则不行
-            ui->tableWidgetScoreBoard->setItem(rowCount,i,new TableWidgetItem(lineList[i]));//自己的继承重载版本
-            qDebug() << lineList[i];
+    }
+
+    //读取小项目奖牌数据到EventList以及header中
+    for (i = 1; i <= EventSum ; i++){
+        lineStr = output.readLine();
+        EventList[i] = lineStr.split('\t');
+        header << EventList[i][0];  //每一行开头第一个元素是项目名称，加入到header表头中
+    }
+
+    //调用actionCalculate来计算国家分数、男女团体分数
+    on_actionCalculate_triggered();
+
+    //根据计算结果填充主页榜单表格,共CountrySum行，4列，4列分别为：国家编号，国家总分，男子总分，女子总分
+    for (i = 1; i <= CountrySum; i++){
+        //i+1即为对应行编号
+        //将计算结果包裹到QStringList lineList中
+        lineList={QString::number(i), QString::number(CountryScore[i]),
+                  QString::number(CountryScoreMale[i]),QString::number(CountryScoreFemale[i])};
+
+        qDebug() << lineList;
+        //用lineList填充表格
+        for (int col = 0; col < 4; col++){
+            ui->tableWidgetScoreBoard->setItem(i-1, col, new TableWidgetItem(lineList[col]));
         }
     }
     curFile.close();//关闭文件
@@ -150,8 +167,8 @@ void OlympicMainWindow::on_actionSave_triggered()
 {
     QFile fileToSave;
     QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"),
-                            "/UntitledOlympicsScoreBoardOutput.txt",
-                            tr("Texts (*.txt)"));
+                                                    "/UntitledOlympicsScoreBoardOutput.txt",
+                                                    tr("Texts (*.txt)"));
     fileToSave.setFileName(fileName);
     //对于QIODevice::truncate,官网文档描述为：
     //If possible, the device is truncated before it is opened. All earlier contents of the device are lost.
@@ -161,34 +178,133 @@ void OlympicMainWindow::on_actionSave_triggered()
         return;
     }
     else{
-        //从tableWidgets中提取当前的排行榜数据并且输出
         QTextStream data(&fileToSave);
         QStringList strList;
+        //将EventList重新组合为输出
+        //定义文件格式如下：
+        //第 1 行 3个整数国家编号 n,男子项目总数 m,女子项目总数 w，用/t分隔
+        //第 2 行到第 m + 1 行，每行开头一个对应的项目名称，之后是 5 个整数 Cij，表示对应的项目 mi 取前五的国家编号 Cij，用/t分隔
+        //第 m + 2 行到第 m + w + 1行，每行开头一个对应的项目名称，之后是 3 个整数 Cij, 表示对应的项目 wi 取前三的国家编号 Cij，用/t分隔
+        //规定 n >= 5 && n <= 300, m >= 0, w >= 0
 
-        //如果取消注释，这一段代码可用于打印表头，可以在未来想要修改程序输出格式时候用到
-        /*
-        for( int c = 0; c < ui->tableWidgetScoreBoard->columnCount(); ++c )
-        {
-            strList <<ui->tableWidgetScoreBoard->horizontalHeaderItem(c)->data(Qt::DisplayRole).toString();
-        }
-        data << strList.join("\t") << "\n";*/
+        //组合第一行
+        strList << QString::number(CountrySum) << QString::number(MaleSum) << QString::number(FemaleSum);
+        data << strList.join("\t") << "\n";
 
-        for( int r = 0; r < ui->tableWidgetScoreBoard->rowCount(); ++r )
-        {
-            strList.clear();
-            for( int c = 0; c < ui->tableWidgetScoreBoard->columnCount(); ++c )
-            {   QTableWidgetItem* item = ui->tableWidgetScoreBoard->item(r,c);        //Load items
-                if (!item || item->text().isEmpty())                        //Test if there is something at item(r,c)
-                {
-                    ui->tableWidgetScoreBoard->setItem(r,c,new QTableWidgetItem("0"));//IF there is nothing write 0
-                }
-                strList << ui->tableWidgetScoreBoard->item( r, c )->text();
-
-            }
-            data << strList.join( "\t" )+"\n";
+        //组合剩余行
+        for (int i = 1; i <= EventSum; i++){//按照头文件定义，EventList[0]没有存放实际数据
+            data << EventList[i].join("\t") << "\n";
         }
         statusBar()->showMessage(tr("File saved successfully."), 3000);
     }
     fileToSave.close();
 }
 
+
+void OlympicMainWindow::on_actionCalculate_triggered()
+{
+    int i = 0;
+    for (i = 1; i <= MaleSum; i++){//统计男子团队数据
+        //假定男子项目均为取前五积分，规则为7、5、3、2、1
+        int num = EventList[i][1].toInt();//获取第1名名次所属国家的编号
+        CountryScoreMale[num] += 7;
+        num = EventList[i][2].toInt();//获取第2名名次所属国家的编号
+        CountryScoreMale[num] += 5;
+        num = EventList[i][3].toInt();//获取第3名名次所属国家的编号
+        CountryScoreMale[num] += 3;
+        num = EventList[i][4].toInt();//获取第3名名次所属国家的编号
+        CountryScoreMale[num] += 2;
+        num = EventList[i][5].toInt();//获取第5名名次所属国家的编号
+        CountryScoreMale[num] += 1;
+    }
+    for (; i <= EventSum; i++){  //统计女子团队数据
+        //假定女子项目均为取前三积分，规则为5、3、2
+        int num = EventList[i][1].toInt();//获取第1名名次所属国家的编号
+        CountryScoreFemale[num] += 5;
+        num = EventList[i][2].toInt();//获取第2名名次所属国家的编号
+        CountryScoreFemale[num] += 3;
+        num = EventList[i][3].toInt();//获取第3名名次所属国家的编号
+        CountryScoreFemale[num] += 2;
+    }
+    for (i = 1; i <= CountrySum; i++){//统计国家数据
+        CountryScore[i] = CountryScoreFemale[i] + CountryScoreMale[i];
+    }
+    return;
+}
+
+
+void OlympicMainWindow::openDefault()
+{
+    //从文件中读取数据，第一步，初始化
+    //QFile f("DefaultInput.txt");
+    curFile.setFileName("DefaultInput.txt");
+    if (! curFile.open(QIODevice::ReadOnly | QIODevice::Text)){
+        qDebug() << "读取文件失败";
+        qDebug()<<"current applicationDirPath: "<<QCoreApplication::applicationDirPath();
+        qDebug()<<"current currentPath: "<<QDir::currentPath();
+    }
+    QTextStream output(&curFile);
+    QHash<QString, QString> varHash;
+    QString lineStr;    //文件的每一行的字符串
+    QStringList lineList;   //整行字符串分割处理为单个字符串存入到表格中
+    varHash.clear();
+    lineList.clear();
+
+    //从文件中读取数据，第二步，遍历文件
+    //定义文件格式如下：
+    //第 1 行 3个整数国家编号 n,男子项目总数 m,女子项目总数 w，用/t分隔
+    //第 2 行到第 m + 1 行，每行开头一个对应的项目名称，之后是 5 个整数 Cij，表示对应的项目 mi 取前五的国家编号 Cij，用/t分隔
+    //第 m + 2 行到第 m + w + 1行，每行开头一个对应的项目名称，之后是 3 个整数 Cij, 表示对应的项目 wi 取前三的国家编号 Cij，用/t分隔
+    //规定 n >= 5 && n <= 300, m >= 0, w >= 0
+
+    //读取n,m,w并存储到对应变量中
+    lineStr = output.readLine();
+    lineList = lineStr.split('\t');
+
+    CountrySum = lineList[0].toInt();
+    MaleSum = lineList[1].toInt();
+    FemaleSum = lineList[2].toInt();
+    EventSum = MaleSum + FemaleSum;
+    int i = 0;
+    //QStringList EventList[400];//最多读取400行，放弃tempLine[0]不使用，也就是最多存储399个子项目数据
+    //int CountryScore[300];
+
+    //初始化国家分数
+    for (i=0;i<300;i++){
+        CountryScore[i] = 0;
+        CountryScoreFemale[i] = 0;
+        CountryScoreMale[i] = 0;
+    }
+
+    //根据国家总数添加对应行
+    for (i = 1; i <= CountrySum; i++){
+        int rowCount = ui->tableWidgetScoreBoard->rowCount();
+        ui->tableWidgetScoreBoard->insertRow(rowCount);
+    }
+
+    //读取小项目奖牌数据到EventList以及header中
+    for (i = 1; i <= EventSum ; i++){
+        lineStr = output.readLine();
+        EventList[i] = lineStr.split('\t');
+        header << EventList[i][0];  //每一行开头第一个元素是项目名称，加入到header表头中
+    }
+
+    //调用actionCalculate来计算国家分数、男女团体分数
+    on_actionCalculate_triggered();
+
+    //根据计算结果填充主页榜单表格,共CountrySum行，4列，4列分别为：国家编号，国家总分，男子总分，女子总分
+    for (i = 1; i <= CountrySum; i++){
+        //i+1即为对应行编号
+        //将计算结果包裹到QStringList lineList中
+        lineList={QString::number(i), QString::number(CountryScore[i]),
+                  QString::number(CountryScoreMale[i]),QString::number(CountryScoreFemale[i])};
+
+        qDebug() << lineList;
+        //用lineList填充表格
+        for (int col = 0; col < 4; col++){
+            ui->tableWidgetScoreBoard->setItem(i-1, col, new TableWidgetItem(lineList[col]));
+        }
+    }
+
+    curFile.close();//关闭文件
+}
--- olympicmainwindow.h
+++ olympicmainwindow.h
@@ -19,7 +19,17 @@ public:
 private:
     Ui::OlympicMainWindow *ui;
     QFile curFile;
+    int CountryScore[300];//[0]不使用
+    int CountryScoreMale[300];//[0]不使用
+    int CountryScoreFemale[300];//[0]不使用
+    int CountrySum;
+    int MaleSum;
+    int FemaleSum;
+    int EventSum;
+    QStringList EventList[400];//EventList[0]不使用
+    QStringList header;//用于存放水平表头数据
     void saveFile(const QString &name);
+    void openDefault();
 
 signals:
     void sectionClicked(int);
@@ -28,5 +38,6 @@ public slots:
 private slots:
     void on_actionOpen_triggered();
     void on_actionSave_triggered();
+    void on_actionCalculate_triggered();
 };
 #endif // OLYMPICMAINWINDOW_H
